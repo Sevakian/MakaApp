6f220d07940ae8c2d799588845defcd767271735
Sevakian <sevakian08@gmail.com>
2024-07-06 15:00:55 +0200

    init

---
+++ src/store/modules/calendar.js
@@ -0,0 +1,19 @@
+import dataContext from "./calendar/dataContext";
+import dataView from "./calendar/dataView";
+import tables from "./calendar/tables";
+import verwContext from "./calendar/verwContext";
+
+export default {
+  namespaced: true,
+
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    dataContext,
+    dataView,
+    tables,
+    verwContext,
+  },
+};
